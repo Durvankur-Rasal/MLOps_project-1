4948f57b835b2627530a1c52eeb48201a681189e
Durvankur <durvankur.rasal48@gmail.com>
2025-12-19 21:15:22 +0530

    project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Durvankur Rasal",
+    author_email="durvankur.rasal48@gmail.com",
+    packages=find_packages()
+)
